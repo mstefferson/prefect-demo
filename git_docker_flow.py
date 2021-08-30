f5b188fb58a95cb0fa95c505c4c20e820ce27a13
Michael Stefferson <mstefferson@gmail.com>
2021-08-29 18:25:29 -0600

    add it back

--- git_docker_flow.py
+++ git_docker_flow.py
@@ -2,17 +2,34 @@ import prefect
 from prefect import Flow, Parameter, task
 from prefect.run_configs import DockerRun
 from prefect.storage import Git, GitHub
-from mega_flow import flow
 
 IMAGE = "prefecthq/prefect:0.15.4"
 # IMAGE = "prefect-dev:latest"
 LOGGER = prefect.context.get("logger")
 
 
+@task
+def summer(x):
+    # this is
+    LOGGER.info("You've been informed")
+    LOGGER.warning("You've been warned")
+    if False:
+        import numpy as np
+        sums = np.sum(x)
+    else:
+        sums = 0
+        for ii in x:
+            sums += ii
+    return sums
+
+with Flow("mega-flow-git-docker") as flow:
+    x = Parameter("x", default=[0, 1, 2])
+    y = summer(x)
+
 def main():
     flow.run()
     flow.run_config = DockerRun(labels=["docker"], image=IMAGE)
-    flow.storage = GitHub(repo="mstefferson/prefect-demo", path="mega_flow.py")
+    flow.storage = GitHub(repo="mstefferson/prefect-demo", path="git_docker_flow.py")
     flow.register("my-demo")
 
 
